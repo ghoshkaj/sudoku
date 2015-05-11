67573f635c0d5e3ed0a271b5a871f3b6abf01955
Kajari Ghosh <ghoshkaj@gmail.com>
2015-05-10 21:03:48 -0400

    added grunt-build-control

--- Gruntfile.js
+++ Gruntfile.js
@@ -9,6 +9,8 @@
 
 module.exports = function (grunt) {
 
+  var pkg = require('./package.json');
+
   // Load grunt tasks automatically
   require('load-grunt-tasks')(grunt);
 
@@ -386,7 +388,37 @@ module.exports = function (grunt) {
         configFile: 'test/karma.conf.js',
         singleRun: true
       }
+    },
+
+    buildcontrol: {
+      options: {
+        dir: 'dist',
+        commit: true,
+        push: true,
+        message: 'Built %sourceName% from commit %sourceCommit% on branch %sourceBranch%'
+      },
+      pages: {
+        options: {
+          remote: 'git@github.com:ghoshkaj/sudoku.git',
+          branch: 'gh-pages'
+        }
+      },
+      // heroku: {
+      //   options: {
+      //     remote: 'git@heroku.com:example-heroku-webapp-1988.git',
+      //     branch: 'master',
+      //     tag: pkg.version
+      //   }
+      // },
+      local: {
+        options: {
+          remote: '../',
+          branch: 'build'
+        }
+      }
     }
+});
+
   });
 
 
