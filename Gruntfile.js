47343ee2b5987499fa4fd6bdac28b697bb3d27e6
Kajari Ghosh <ghoshkaj@gmail.com>
2015-05-10 21:42:25 -0400

    fixed gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -419,8 +419,6 @@ module.exports = function (grunt) {
     }
 });
 
-  });
-
 
   grunt.registerTask('serve', 'Compile then start a connect web server', function (target) {
     if (target === 'dist') {
